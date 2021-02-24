84ea6bd70c00763038d50e32dfe00375214e6114
rschiewer <r.schiewer@gmx.de>
2021-02-24 22:19:06 +0100

    Restructure of code

--- main_conv_predictor.py
+++ main_conv_predictor.py
@@ -1,8 +1,11 @@
+from old_predictors import AutoregressiveMultiHeadFullyConvolutionalPredictor, \
+    AutoregressiveProbabilisticFullyConvolutionalPredictor
 from predictors import *
 #from vq_vae import *
 from keras_vq_vae import VectorQuantizerEMAKeras
 from replay_memory_tools import *
 from matplotlib import pyplot as plt
+from blockworld import *
 
 #os.environ['TF_CPP_MIN_LOG_LEVEL']='0'
 
@@ -18,9 +21,10 @@ if gpus:
         print(e)
 
 
-def vq_vae_net(n_embeddings, d_embeddings, train_data_var, frame_stack=1):
+def vq_vae_net(n_embeddings, d_embeddings, train_data_var, grayscale_input=False, frame_stack=1):
     assert frame_stack == 1, 'No frame stacking supported currently'
-    vae = VectorQuantizerEMAKeras(train_data_var, num_embeddings=n_embeddings, embedding_dim=d_embeddings)
+    vae = VectorQuantizerEMAKeras(train_data_var, num_embeddings=n_embeddings, embedding_dim=d_embeddings,
+                                  grayscale_input=grayscale_input)
     vae.compile(optimizer=tf.optimizers.Adam())
     return vae
 
@@ -147,11 +151,12 @@ def prepare_predictor_data(trajectories, vae, n_steps, n_warmup_steps):
 def train_predictor(vae, predictor, trajectories, n_train_steps, n_traj_steps, n_warmup_steps,  predictor_weights_path, steps_per_epoch=200, batch_size=32):
     encoded_obs, encoded_next_obs, rewards, actions = prepare_predictor_data(trajectories, vae, n_traj_steps, n_warmup_steps)
 
-    for i in range(len(encoded_obs) - 1):
-        assert np.sum(encoded_obs[i + 1] - encoded_next_obs[i]) <= 0.0001
+    # sanity check
+    for i_t in range(n_warmup_steps - 1):
+        assert np.isclose(encoded_obs[:, i_t + 1], encoded_next_obs[:, i_t], atol=0.001).all(), 'Trajectory seems to be corrupted'
 
     dataset = (tf.data.Dataset.from_tensor_slices(((encoded_obs, actions), (encoded_next_obs, rewards)))
-               .shuffle(steps_per_epoch * batch_size)
+               .shuffle(100000)
                .repeat(-1)
                .batch(batch_size, drop_remainder=True)
                .prefetch(-1))
@@ -204,24 +209,25 @@ def load_predictor_weights(complete_predictors_list, env_names, plots=False):
 
 def single_predictor():
     # env #
-    #env_names = ['Gridworld-partial-room-v0', 'Gridworld-partial-room-v1', 'Gridworld-partial-room-v2']
-    env_names = ['BoxingNoFrameskip-v0', 'SpaceInvadersNoFrameskip-v0', 'DemonAttackNoFrameskip-v0']
+    env_names = ['Gridworld-partial-room-v0', 'Gridworld-partial-room-v1', 'Gridworld-partial-room-v2']
+    #env_names = ['BoxingNoFrameskip-v0', 'SpaceInvadersNoFrameskip-v0', 'DemonAttackNoFrameskip-v0']
     obs_resize = (84, 84)
-    collect_samples_per_env = 80000
+    grayscale_input = False
+    collect_samples_per_env = 50000
 
     # vae params #
-    n_vae_steps = 80000
+    n_vae_steps = 40000
     n_embeddings = 128
     d_embedding = 32
     frame_stack = 1
 
     # predictor params #
-    n_pred_train_steps = 5000
-    n_subtrajectories = 3000
-    n_traj_steps = 20
-    n_warmup_steps = 5
+    n_pred_train_steps = 10000
+    n_subtrajectories = 5000
+    n_traj_steps = 10
+    n_warmup_steps = 3
 
-    tf.config.run_functions_eagerly(True)
+    #tf.config.run_functions_eagerly(True)
 
     # start training procedure #
 
@@ -230,9 +236,9 @@ def single_predictor():
     vae_weights_path = 'vae_model' + '_and_'.join(env_names) + '_vae_weights'
     predictor_weights_path = 'predictor_model' + '_and_'.join(env_names) + '_predictor_weights'
 
-    #envs = [gym.make(env_name) for env_name in env_names]
+    envs = [gym.make(env_name) for env_name in env_names]
     #envs = [gym.wrappers.GrayScaleObservation(gym.wrappers.ResizeObservation(gym.make(env_name), obs_resize), keep_dim=True) for env_name in env_names]
-    envs = [gym.wrappers.AtariPreprocessing(gym.make(env_name), grayscale_newaxis=True) for env_name in env_names]
+    #envs = [gym.wrappers.AtariPreprocessing(gym.make(env_name), grayscale_newaxis=True) for env_name in env_names]
     obs_shape = envs[0].observation_space.shape
     n_actions = envs[0].action_space.n
 
@@ -243,7 +249,7 @@ def single_predictor():
     mix_memory = load_env_samples(mix_mem_path)
     train_data_var = np.var(mix_memory['s'][0] / 255)
 
-    vae = vq_vae_net(n_embeddings, d_embedding, train_data_var, frame_stack)
+    vae = vq_vae_net(n_embeddings, d_embedding, train_data_var, grayscale_input, frame_stack)
     vae_index_matrix_shape = vae.compute_latent_shape(mix_memory['s'])
     all_predictor = AutoregressiveProbabilisticFullyConvolutionalMultiHeadPredictor(vae_index_matrix_shape, n_actions,
                                                                                     vae,
@@ -257,80 +263,63 @@ def single_predictor():
     # train vae
     #load_vae_weights(vae, mix_memory, file_name=vae_weights_path, plots=False)
     #train_vae(vae, mix_memory, n_vae_steps, file_name=vae_weights_path, batch_size=512)
-    load_vae_weights(vae, mix_memory, file_name=vae_weights_path, plots=True)
+    load_vae_weights(vae, mix_memory, file_name=vae_weights_path, plots=False)
 
     # extract trajectories and train predictor
     trajs = extract_subtrajectories(mix_memory, n_subtrajectories, n_traj_steps, False)
 
     #all_predictor.load_weights('predictors/' + predictor_weights_path)
-    train_predictor(vae, all_predictor, trajs, n_pred_train_steps, n_traj_steps, n_warmup_steps, predictor_weights_path, batch_size=50)
+    train_predictor(vae, all_predictor, trajs, n_pred_train_steps, n_traj_steps, n_warmup_steps, predictor_weights_path, batch_size=32)
     all_predictor.load_weights('predictors/' + predictor_weights_path).expect_partial()
 
-    generate_test_rollouts(all_predictor, mix_memory, vae, 100, 1, 'Predictor Test')
+    targets, rollouts, w_predictors = generate_test_rollouts(all_predictor, mix_memory, vae, 200, 1, 4)
+    rollout_videos(targets,rollouts, w_predictors, 'Predictor Test')
+
+    pixel_diff_mean = np.mean(targets - rollouts, axis=(0, 2, 3, 4))
+    pixel_diff_var = np.std(targets - rollouts, axis=(0, 2, 3, 4))
+    x = range(len(pixel_diff_mean))
+    plt.plot(x, pixel_diff_mean)
+    plt.fill_between(x, pixel_diff_mean - pixel_diff_var, pixel_diff_mean + pixel_diff_var, alpha=0.2)
+    plt.show()
 
 
-def generate_test_rollouts(predictor, mem, vae, n_steps, n_warmup_steps, video_title):
-    n_trajectories = 5
+def generate_test_rollouts(predictor, mem, vae, n_steps, n_warmup_steps, n_trajectories):
+    if not predictor.open_loop_rollout_training:
+        n_warmup_steps = n_steps
 
     trajectories = extract_subtrajectories(mem, n_trajectories, n_steps, False)
     encoded_obs, _, _, actions = prepare_predictor_data(trajectories, vae, n_steps, n_warmup_steps)
 
     next_obs = trajectories['s_']
-    targets = next_obs[:, n_warmup_steps - 1:]
+    if not predictor.open_loop_rollout_training:
+        targets = next_obs[:, :n_warmup_steps]
+    else:
+        targets = next_obs[:, n_warmup_steps - 1:]
     encoded_start_obs = encoded_obs[:, :n_warmup_steps]
 
     #print([env_idx.shape, encoded_start_obs.shape, one_hot_acts.shape])
 
     # do rollout
     o_rollout, r_rollout, w_predictors = predictor([encoded_start_obs, actions])
-
-    #w_predictors = tf.transpose(w_predictors, [1, 2, 0]).numpy()
-
     chosen_predictor = np.argmax(tf.transpose(w_predictors, [1, 2, 0]), axis=-1)
+    decoded_rollout_obs = cast_and_unnormalize_images(vae.decode_from_indices(o_rollout)).numpy()
+
+    return targets, decoded_rollout_obs, chosen_predictor
+
+
+def rollout_videos(targets, decoded_rollout_obs, chosen_predictor, video_title, store_animation=False):
     max_pred_idx = chosen_predictor.max()
-
-    decoded_rollout_obs = cast_and_unnormalize_images(vae.decode_from_indices(o_rollout, 1))
-
-    # remove batch dimension since it's 1 anyway
     all_videos = []
     all_titles = []
     for i, (ground_truth, rollout, pred_weight) in enumerate(zip(targets, decoded_rollout_obs, chosen_predictor)):
         weight_imgs = np.stack([np.full_like(ground_truth[0], w) / max_pred_idx * 255 for w in pred_weight])
         all_videos.extend([ground_truth, rollout, weight_imgs])
         all_titles.extend([f'true {i}', f'rollout {i}', f'weight {i}'])
-
     anim = trajectory_video(all_videos, all_titles, overall_title=video_title, max_cols=3)
-    #writer = animation.writers['ffmpeg'](fps=10, bitrate=1800)
-    #anim.save('rollout.mp4', writer=writer)
 
-
-def estimate_performance(predictor, mem, use_env_idx, vae, n_steps, n_warmup_steps, n_trajectories):
-    predictor.predict_steps = n_steps
-
-    trajectories = extract_subtrajectories(mem, n_trajectories, n_steps + n_warmup_steps - 1, False)
-    encoded_obs, _, _, actions = prepare_predictor_data(trajectories, vae, n_steps, n_warmup_steps)
-
-    next_obs = trajectories['s_'] / 255
-    targets = next_obs[:, 0:n_steps]
-    encoded_start_obs = encoded_obs[:, 0:n_warmup_steps]
-
-    if use_env_idx:
-        env_idx = trajectories['env'][:, 0]
-    else:
-        env_idx = np.zeros_like(trajectories['env'][:, 0])
-
-    # do rollout
-    rollout = predictor.predict([env_idx, encoded_start_obs, actions], n_steps)
-    decoded_rollout_obs = np.clip(vae.decode(rollout, 'indices'), 0, 1)
-
-    # find player pos
-    #analyzed_player_pos = pos_finder.predict(decoded_rollout_obs).astype(int)
-    #pos_diff = np.abs(end_pos - analyzed_player_pos)
-    #pos_diff /= 2
-    # debug_img = np.repeat(pos_diff, 3, axis=1).reshape(-1, 1, 2, 3)
-    diff_img = np.clip(np.abs(decoded_rollout_obs - targets), 0, 1)
-
-    return diff_img.mean(axis=(0, 2, 3, 4))
+    if store_animation:
+        writer = animation.writers['ffmpeg'](fps=10, bitrate=1800)
+        anim.save('rollout.mp4', writer=writer)
 
 
 if __name__ == '__main__':

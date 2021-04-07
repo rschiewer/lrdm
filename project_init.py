48161e16069a0b859c356c8a28bf639817fdaadf
Robin Schiewer <robin.schiewer@ini.rub.de>
2021-04-07 10:42:03 +0200

    Split up config file in multiple individual files
    Commit type: Refactoring

--- project_init.py
+++ project_init.py
@@ -3,23 +3,33 @@ from yamldataclassconfig import create_file_path_field
 from yamldataclassconfig.config import YamlDataClassConfig
 from pathlib import Path
 import tensorflow as tf
+import yaml
 from typing import Union, List
 
 
-# see https://www.tensorflow.org/guide/gpu
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-    try:
-        # Currently, memory growth needs to be the same across GPUs
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-    except RuntimeError as e:
-        # Memory growth must be set before GPUs have been initialized
-        print(e)
+class MultiYamlDataClassConfig(YamlDataClassConfig):
+
+    def load(self, file_paths: Union[Path, str, List[Path], List[str]] = None, path_is_absolute: bool = False):
+        """
+        This method loads from YAML file to properties of self instance.
+        Why doesn't load when __init__ is to make the following requirements compatible:
+        1. Access config as global
+        2. Independent on config for development or use config for unit testing when unit testing
+        """
+        if file_paths is None:
+            file_paths = self.FILE_PATH
+
+        if type(file_paths) is Path or type(file_paths) is str:
+            file_paths = [file_paths]
+
+        dict_config = {}
+        for file_path in file_paths:
+            dict_config.update(yaml.full_load(Path(file_path).read_text('UTF-8')))
+        self.__dict__.update(self.__class__.schema().load(dict_config).__dict__)
 
 
 @dataclass
-class ExperimentConfig(YamlDataClassConfig):
+class ExperimentConfig(MultiYamlDataClassConfig):
     tf_eager_mode: bool = None
     model_summaries: bool = None
     neptune_project_name: str = None
@@ -56,11 +66,24 @@ class ExperimentConfig(YamlDataClassConfig):
     pred_weights_path: str = None
     pred_train_stats_path: str = None
 
-    #FILE_PATH: Path = create_file_path_field(Path(__file__).parent / 'config_1_pred.yml')
+    ctrl_n_runs: int = None
+    ctrl_n_plan_steps: int = None
+    ctrl_n_rollouts: int = None
+    ctrl_n_iterations: int = None
+    crtl_top_perc: float = None
+    ctrl_gamma: float = None
+    ctrl_do_mpc: bool = None
+    ctrl_render: bool = None
+
+    #FILE_PATH: Path = create_file_path_field(Path(__file__).parent / 'config_general.yml')
 
 
-CONFIG = ExperimentConfig()
-CONFIG.load('config_1_pred.yml')
+def load_config_files(file_paths):
+    dict_config = {}
+    for file_path in file_paths:
+        dict_config.update(yaml.full_load(Path(file_path).read_text('UTF-8')))
+
+
 
 
 # check if all directories for logging and storing stuff exist, generate if not present
@@ -94,3 +117,21 @@ def gen_predictor_weights_path(env_names):
 def gen_predictor_train_stats_path(env_names):
     return CONFIG.pred_train_stats_path + '_and_'.join(env_names) + '_' + str(CONFIG.pred_n_models)
 
+
+# Global options and settings ------------------------------------------------------------------------------------------
+
+
+# see https://www.tensorflow.org/guide/gpu
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
+
+CONFIG = ExperimentConfig()
+CONFIG.load(['config_general.yml', 'config_control.yml', 'config_predictor_1_mdl.yml', 'config_vae.yml'])

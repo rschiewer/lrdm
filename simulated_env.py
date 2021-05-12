45c98907249894ea65b7d31430df0a2239e1ffdd
Robin Schiewer <robin.schiewer@ini.rub.de>
2021-05-12 19:49:48 +0200

    Implemented TaskEnv class that wraps an env to augment its observation with a task ID integer
    Commit type: Feature, Refactoring

--- simulated_env.py
+++ simulated_env.py
@@ -9,16 +9,52 @@ import matplotlib.pyplot as plt
 from typing import List, Union, Tuple, Optional
 
 
-class LatentSpaceEnv(gym.Env):
+class TaskEnv(gym.Env):
 
-    def __init__(self, orig_env: gym.Env, vae: VectorQuantizerEMAKeras, task_id: int = None):
-        super(LatentSpaceEnv, self).__init__()
+    def __init__(self, orig_env: gym.Env, task_id: Union[int, None] = None):
+        super(TaskEnv, self).__init__()
+        if task_id is not None:
+            assert task_id < 255, 'Maximum of 255 tasks'
+
+        self._orig_env = orig_env
+        self._task_id = task_id
+        self.action_space = orig_env.action_space
+        if task_id is None:
+            self.observation_space = orig_env.observation_space
+        else:
+            self.observation_space = spaces.Dict({'o': orig_env.observation_space,
+                                                  'task': spaces.Box(low=0, high=254, shape=(), dtype=np.uint8)})
+
+    @property
+    def unwrapped(self):
+        return self._orig_env
+
+    def _build_complete_obs(self, obs):
+        if self._task_id is None:
+            return tf.cast(obs, tf.float32)
+        else:
+            return {'o': tf.cast(obs, tf.float32), 'task': self._task_id}
+
+    def step(self, action):
+        o, r, done, info = self._orig_env.step(action)
+        return self._build_complete_obs(o), r, done, info
+
+    def reset(self):
+        o = self._orig_env.reset()
+        return self._build_complete_obs(o)
+
+    def render(self, mode='human'):
+        self._orig_env.render(mode)
+
+
+class LatentSpaceEnv(TaskEnv):
+
+    def __init__(self, orig_env: gym.Env, vae: VectorQuantizerEMAKeras, task_id: Union[int, None] = None):
+        super(LatentSpaceEnv, self).__init__(orig_env, task_id)
 
         self._vae = vae
-        self._orig_env = orig_env
         self._plot_win = None
         self._current_state = None
-        self._task_id = task_id
 
         self.action_space = orig_env.action_space
         obs_shape = vae.compute_latent_shape(orig_env.observation_space.shape) + (vae.embedding_dim,)
@@ -27,7 +63,7 @@ class LatentSpaceEnv(gym.Env):
         else:
             self.observation_space = spaces.Dict(
                 {'o': spaces.Box(low=0, high=vae.num_embeddings, shape=obs_shape, dtype=np.float32),
-                 'env': spaces.Box(low=0, high=254, shape=(), dtype=np.uint8)})
+                 'task': spaces.Box(low=0, high=254, shape=(), dtype=np.uint8)})
 
     def _encode_image_obs(self, obs):
         normalized_obs = cast_and_normalize_images(obs)
@@ -37,12 +73,6 @@ class LatentSpaceEnv(gym.Env):
     def _decode_image_obs(self, embedding):
         decoded = self._vae.decode_from_vectors(embedding[np.newaxis, np.newaxis, ..., 0])
         return cast_and_unnormalize_images(decoded[0, 0, ...])
-
-    def _build_complete_obs(self, obs):
-        if self._task_id is None:
-            return tf.cast(obs, tf.float32)
-        else:
-            return {'o': tf.cast(obs, tf.float32), 'env': self._task_id}
 
     def reset(self):
         original_obs = self._orig_env.reset()
@@ -84,11 +114,6 @@ class SimulatedLatentSpaceEnv(LatentSpaceEnv):
 
         self._simulator = simulator
         self._done_threshold = done_threshold
-
-    #def reset(self):
-    #    complete_start_obs = super(SimulatedLatentSpaceEnv, self).reset()
-    #    self._current_state = complete_start_obs['o']
-    #    return complete_start_obs
 
     def step(self, action):
         assert self.action_space.contains(action), f'Action {action} is not part of the action space'
@@ -145,7 +170,7 @@ class MultiEnv(gym.Env):
             assert env.observation_space == self.observation_space, 'All envs must have the same observation space'
 
     def _gen_envs(self):
-        return [env for env in self._orig_envs]
+        return [TaskEnv(env, tid) for env, tid in zip(self._orig_envs, self._task_ids)]
 
     def reset(self, task_id: int = None):
         if task_id is None:
@@ -168,7 +193,7 @@ class MultiLatentSpaceEnv(MultiEnv):
         super(MultiLatentSpaceEnv, self).__init__(orig_envs, task_ids)
 
     def _gen_envs(self):
-        return [LatentSpaceEnv(env, self._vae, id) for env, id in zip(self._orig_envs, self._task_ids)]
+        return [LatentSpaceEnv(env, self._vae, tid) for env, tid in zip(self._orig_envs, self._task_ids)]
 
 
 class MultiSimulatedLatentSpaceEnv(MultiLatentSpaceEnv):
@@ -180,4 +205,4 @@ class MultiSimulatedLatentSpaceEnv(MultiLatentSpaceEnv):
         super(MultiSimulatedLatentSpaceEnv, self).__init__(orig_envs, vae, task_ids)
 
     def _gen_envs(self):
-        return [SimulatedLatentSpaceEnv(env, self._simulator, self._vae, id) for env, id in zip(self._orig_envs, self._task_ids)]
+        return [SimulatedLatentSpaceEnv(env, self._simulator, self._vae, tid) for env, tid in zip(self._orig_envs, self._task_ids)]

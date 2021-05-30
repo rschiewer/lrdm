4e3b90491abe17b9c9d03bc797b66f7f2c84d4aa
Robin Schiewer <robin.schiewer@ini.rub.de>
2021-05-30 19:22:37 +0200

    Plotting average steps and rewards per environment now works with multiple runs per predictor per env
    Commit type: Refactoring

--- gen_statistics.py
+++ gen_statistics.py
@@ -7,63 +7,52 @@ import seaborn as sns
 sns.set_theme(palette='muted')
 
 
-def plot_avg_steps(project_name, env_names, filter_ids, filter_tags, alternative_task_names=None, alternative_labels=None):
+def plot_avg_steps(project_name, env_names, filter_ids, filter_tags, model_labels, task_names):
     project = neptune.get_project(project_name)
     runs = project.fetch_runs_table(tag=filter_tags, id=filter_ids).to_pandas()
     print(f'Found {len(runs)} runs...')
 
     # collect data
-    df = pd.DataFrame(columns=['run_id', 'n_models', 'environment', 'steps_mean', 'steps_std', 'r_mean', 'r_std'])
+    df = pd.DataFrame(columns=['run_id', 'n_models', 'environment', 'steps', 'reward'])
     for i, run_metadata in runs.iterrows():
         run = neptune.init(project=project_name, run=run_metadata['sys/id'])
 
         for env_name in env_names:
             steps = run[f'{env_name}/steps'].fetch_values()['value'].to_numpy()
             rewards = run[f'{env_name}/rewards'].fetch_values()['value'].to_numpy()
-            row = {'run_id': run_metadata['sys/id'], 'n_models': run["parameters/pred_n_models"].fetch(),
-                   'environment': env_name, 'steps_mean': steps.mean(), 'steps_std': steps.std(),
-                   'r_mean': rewards.mean(), 'r_std': rewards.std()}
-            df = df.append(row, ignore_index=True)
+            for step, reward in zip(steps, rewards):
+                row = {'run_id': run_metadata['sys/id'], 'n_models': run["parameters/pred_n_models"].fetch(),
+                       'environment': env_name, 'steps': step, 'reward': reward}
+                df = df.append(row, ignore_index=True)
 
     # plotting
     x = np.arange(len(env_names))
-    n_ids = len(filter_ids)
     width = 0.35
     fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(12, 4))
 
-
     n_exp_setups = len(df.groupby('n_models'))
-    for _, grouped_by_env in df.groupby('environment'):
-        for i, (name, grouped_by_n_models) in enumerate(grouped_by_env.groupby('n_models')):
-            steps_mean = grouped_by_n_models['steps_mean'].mean()
-            steps_std = grouped_by_n_models[]
-            ax0.bar(x - width/2 * (n_exp_setups-1) + width * i, run_data['steps_mean'], width, yerr=run_data['steps_std'], label=label)
-            ax1.bar(x - width/2 * (n_exp_setups-1) + width * i, run_data['r_mean'], width, yerr=run_data['r_std'], label=label)
-
-    for i, id in enumerate(filter_ids):
-        run_data = df.loc[df['run_id'] == id]
-        if alternative_labels:
-            label = alternative_labels[i]
-        else:
-            label = f'{run_data["n_models"].mean()}'
-        ax0.bar(x - width/2 * (n_ids-1) + width * i, run_data['steps_mean'], width, yerr=run_data['steps_std'], label=label)
-        ax1.bar(x - width/2 * (n_ids-1) + width * i, run_data['r_mean'], width, yerr=run_data['r_std'], label=label)
-        ax0.grid(True, fillstyle='bottom', alpha=0.5)
-        ax1.grid(True, fillstyle='bottom', alpha=0.5)
+    for i, (name, grouped_by_n_models) in enumerate(df.groupby('n_models')):
+        r_means = []
+        r_stds = []
+        steps_means = []
+        steps_stds = []
+        for _, grouped_by_env in grouped_by_n_models.groupby('environment'):
+            steps_means.append(grouped_by_env['steps'].mean())
+            steps_stds.append(grouped_by_env['steps'].std())
+            r_means.append(grouped_by_env['reward'].mean())
+            r_stds.append(grouped_by_env['reward'].std())
+        ax0.bar(x - width/2 * (n_exp_setups-1) + width * i, steps_means, width, yerr=steps_stds, label=model_labels[name])
+        ax1.bar(x - width/2 * (n_exp_setups-1) + width * i, r_means, width, yerr=r_stds, label=model_labels[name])
+    ax0.grid(True, fillstyle='bottom', alpha=0.5)
+    ax1.grid(True, fillstyle='bottom', alpha=0.5)
 
     ax0.set_ylabel('Average Steps')
     ax0.set_xticks(x)
-    if alternative_task_names:
-        ax0.set_xticklabels(alternative_task_names)
-    else:
-        ax0.set_xticklabels(env_names)
+    ax0.set_xticklabels(task_names)
 
     ax1.set_ylabel('Average Rewards')
     ax1.set_xticks(x)
-    if alternative_task_names:
-        ax1.set_xticklabels(alternative_task_names)
-    else:
-        ax1.set_xticklabels(env_names)
+    ax1.set_xticklabels(task_names)
 
     ax1.legend()
     fig.tight_layout()
@@ -133,6 +122,7 @@ if __name__ == '__main__':
     project_name = 'rschiewer/predictor'
     env_names = ['Gridworld-partial-room-v3', 'Gridworld-partial-room-v4', 'Gridworld-partial-room-v5']
 
-    plot_avg_steps(project_name, env_names, ['PRED-643', 'PRED-642', 'PRED-666', 'PRED-667'], ['control'], ['Task 1', 'Task 2', 'Task 3'], ['monolithic RDM', 'multi RDM'])
+    plot_avg_steps(project_name, env_names, ['PRED-643', 'PRED-642', 'PRED-666', 'PRED-667'], ['control'],
+                   {1: 'monolithic RDM', 3: 'multi RDM'}, ['Task 1', 'Task 2', 'Task 3'])
     #plot_loss(project_name, ['PRED-635', 'PRED-637'], ['predictor'], False, ['monolithic RDM', 'multi RDM'])
     #plot_picker_loss(project_name, ['PRED-635', 'PRED-637'], ['predictor'], False, ['monolithic RDM', 'multi RDM'])

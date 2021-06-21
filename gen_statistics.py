92eb1db7a5e44edf48269ad8c3a923c32fb559d6
Robin Schiewer <robin.schiewer@ini.rub.de>
2021-06-21 15:36:32 +0200

    Added proper titles, axis labels and shaded error areas together with the ability to average over n runs to the plotting functions
    Commit type: Feature, Refactoring

--- gen_statistics.py
+++ gen_statistics.py
@@ -46,20 +46,23 @@ def plot_avg_steps(project_name, env_names, filter_ids, filter_tags, model_label
     ax0.grid(True, fillstyle='bottom', alpha=0.5)
     ax1.grid(True, fillstyle='bottom', alpha=0.5)
 
-    ax0.set_ylabel('Average Steps')
+    ax0.set_title('Average Steps')
+    ax0.set_ylabel('Average Steps (Lower is Better)')
     ax0.set_xticks(x)
     ax0.set_xticklabels(task_names)
 
-    ax1.set_ylabel('Average Rewards')
+    ax1.set_title('Average Rewards')
+    ax1.set_ylabel('Average Rewards (Higher is Better)')
     ax1.set_xticks(x)
     ax1.set_xticklabels(task_names)
 
     ax1.legend()
     fig.tight_layout()
-    plt.show()
+    plt.savefig('rewards and steps per env.png', dpi=300)
+    #plt.show()
 
 
-def plot_loss(project_name, filter_ids, filter_tags, log_scale=False, alternative_labels=None):
+def plot_loss(project_name, filter_ids, filter_tags, labels, log_scale=False):
     project = neptune.get_project(project_name)
     runs = project.fetch_runs_table(tag=filter_tags, id=filter_ids).to_pandas()
     print(f'Found {len(runs)} runs...')
@@ -71,24 +74,30 @@ def plot_loss(project_name, filter_ids, filter_tags, log_scale=False, alternativ
         row = {'run_id': run_metadata['sys/id'], 'n_models': run["parameters/pred_n_models"].fetch(), 'loss': loss}
         df = df.append(row, ignore_index=True)
 
-    for i, row in enumerate(df.iterrows()):
-        if alternative_labels:
-            label = alternative_labels[i]
-        else:
-            label = f'{row[1]["n_models"]} models'
-        plt.plot(row[1]['loss'], label=label)
+    fig = plt.figure(figsize=(6, 4))
+    for i, (name, grouped_by_n_models) in enumerate(df.groupby('n_models')):
+        loss_vals = grouped_by_n_models['loss'].to_numpy()
+        loss_vals = np.array([arr for arr in loss_vals])
+        x = range(len(loss_vals[0]))
+        y = loss_vals.mean(axis=0)
+        y_std = loss_vals.std(axis=0)
+        plt.plot(x, y, label=labels[name])
+        plt.fill_between(x, y - y_std, y + y_std, alpha=0.3)
     if log_scale:
         plt.yscale('log')
-        plt.title('RDM loss (log scale)')
+        #plt.title('RDM loss (log scale)')
     else:
-        plt.title('RDM loss')
-    plt.ylabel('loss')
-    plt.xlabel('epochs')
+        pass
+        #plt.title('RDM loss')
+    plt.suptitle('RDM Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epochs')
     plt.legend()
-    plt.show()
+    plt.savefig('RDM loss.png', dpi=300)
+    #plt.show()
 
 
-def plot_picker_loss(project_name, filter_ids, filter_tags, log_scale=False, alternative_labels=None):
+def plot_picker_loss(project_name, filter_ids, filter_tags, labels, log_scale=False):
     project = neptune.get_project(project_name)
     runs = project.fetch_runs_table(tag=filter_tags, id=filter_ids).to_pandas()
     print(f'Found {len(runs)} runs...')
@@ -100,6 +109,29 @@ def plot_picker_loss(project_name, filter_ids, filter_tags, log_scale=False, alt
         row = {'run_id': run_metadata['sys/id'], 'n_models': run["parameters/pred_n_models"].fetch(), 'picker_loss': loss}
         df = df.append(row, ignore_index=True)
 
+    fig = plt.figure(figsize=(6, 4))
+    for i, (name, grouped_by_n_models) in enumerate(df.groupby('n_models')):
+        loss_vals = grouped_by_n_models['picker_loss'].to_numpy()
+        loss_vals = np.array([arr for arr in loss_vals])
+        x = range(len(loss_vals[0]))
+        y = loss_vals.mean(axis=0)
+        y_std = loss_vals.std(axis=0)
+        plt.plot(x, y, label=labels[name])
+        plt.fill_between(x, y - y_std, y + y_std, alpha=0.3)
+    if log_scale:
+        plt.yscale('log')
+        #plt.title('Task Classification Loss (log scale)')
+    else:
+        pass
+        #plt.title('Task Classification Loss')
+    plt.suptitle('Task Classification Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epochs')
+    plt.legend()
+    plt.savefig('TCN loss.png', dpi=300)
+    #plt.show()
+
+    """
     #fig, ax = plt.subplots()
     for i, row in enumerate(df.iterrows()):
         if alternative_labels:
@@ -116,13 +148,15 @@ def plot_picker_loss(project_name, filter_ids, filter_tags, log_scale=False, alt
     plt.xlabel('epochs')
     plt.legend()
     plt.show()
+    """
 
 
 if __name__ == '__main__':
     project_name = 'rschiewer/predictor'
     env_names = ['Gridworld-partial-room-v3', 'Gridworld-partial-room-v4', 'Gridworld-partial-room-v5']
 
-    plot_avg_steps(project_name, env_names, ['PRED-643', 'PRED-642', 'PRED-666', 'PRED-667'], ['control'],
-                   {1: 'monolithic RDM', 3: 'multi RDM'}, ['Task 1', 'Task 2', 'Task 3'])
-    #plot_loss(project_name, ['PRED-635', 'PRED-637'], ['predictor'], False, ['monolithic RDM', 'multi RDM'])
-    #plot_picker_loss(project_name, ['PRED-635', 'PRED-637'], ['predictor'], False, ['monolithic RDM', 'multi RDM'])
+    plot_avg_steps(project_name, env_names, ['PRED-643', 'PRED-642', 'PRED-666', 'PRED-667', 'PRED-680', 'PRED-679'],
+                   ['control'], {1: 'monolithic RDM', 3: 'multi RDM'}, ['Task 1', 'Task 2', 'Task 3'])
+
+    #plot_loss(project_name, ['PRED-635', 'PRED-637', 'PRED-660', 'PRED-659', 'PRED-676', 'PRED-677'], ['predictor'], {1: 'monolithic RDM', 3: 'multi RDM'}, False)
+    #plot_picker_loss(project_name, ['PRED-635', 'PRED-637', 'PRED-660', 'PRED-659', 'PRED-676', 'PRED-677'], ['predictor'], {1: 'monolithic RDM', 3: 'multi RDM'}, False)
